9e98681e5dd09ae0189895afb246b074996adc7f
tuantv <tuan.tv.social@gmail.com>
2021-11-27 11:11:55 +0700

    add error hanler

--- demo/src/main/java/com/example/demo/controller/ProductController.java
+++ demo/src/main/java/com/example/demo/controller/ProductController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.ProductBriefDTO;
 import com.example.demo.dto.ProductDetailDTO;
 import com.example.demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/products")
