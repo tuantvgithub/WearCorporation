919df9272184f40aca5d7b34eb68d633898f6ccc
tuantvgithub <tuan.tv.social@gmail.com>
2022-01-22 15:56:25 +0700

    del folder demo

--- demo/src/main/java/com/example/demo/proxies/UserWebServiceProxy.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.proxies;
-
-import com.example.demo.bean.UserBean;
-
-public interface UserWebServiceProxy {
-   public UserBean getUserById(int userId); 
-}
--- demo/src/main/java/com/example/demo/service/product/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service.product;
-
-import com.example.demo.dto.product.ProductDetailDTO;
-
-public interface ProductService {
-
-    ProductDetailDTO getProductDetailDTOById(Long productId);
-
-}
