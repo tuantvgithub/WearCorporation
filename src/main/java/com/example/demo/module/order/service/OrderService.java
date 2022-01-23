b51b533f78a3285b03e432ac25250bdb184e7e8f
quanpx <quanphungxuan00@gmail.com>
2022-01-24 01:19:58 +0700

    update get role

--- src/main/java/com/example/demo/module/order/service/OrderService.java
+++ src/main/java/com/example/demo/module/order/service/OrderService.java
@@ -2,6 +2,7 @@ package com.example.demo.module.order.service;
 
 import com.example.demo.client_ui.dto.order.OrderBriefDTO;
 import com.example.demo.client_ui.dto.order.OrderDetailDTO;
+import com.example.demo.module.order.bean.OrderRequestBean;
 
 import java.util.List;
 
@@ -10,5 +11,5 @@ public interface OrderService {
     List<OrderBriefDTO> getAllOrderDTOByUserId(Integer userId);
     OrderDetailDTO getOrderDTOById(Integer orderId);
     void updateOrder(Integer orderId,OrderDetailDTO updateOder);
-    OrderDetailDTO createOrder(OrderDetailDTO orderDetailDTO);
+    OrderDetailDTO createOrder(OrderRequestBean orderDetailDTO);
 }
